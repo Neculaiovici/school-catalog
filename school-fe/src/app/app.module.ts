4a2e6909f558d70e060abbeffd76248189d7a436
Neculaiovici <mocanu.robert.n@gmail.com>
2024-02-01 01:51:24 +0200

    adding material (#5)
    Co-authored-by: INTEGRISOFT\robert.mocanu <robert.mocanu@integrisoft.ro>

--- school-fe/src/app/app.module.ts
+++ school-fe/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { FooterComponent } from './common/footer/footer.component';
+import { NavigationComponent } from './common/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule, MatButtonModule, MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
