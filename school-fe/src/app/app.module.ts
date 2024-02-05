b66044e6274fef1eab28662ee4715a7614181378
INTEGRISOFT\robert.mocanu <robert.mocanu@integrisoft.ro>
2024-02-05 23:57:59 +0200

    adding tooltip

--- school-fe/src/app/app.module.ts
+++ school-fe/src/app/app.module.ts
@@ -5,16 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { FooterComponent } from './common/footer/footer.component';
 import { NavigationComponent } from './common/navigation/navigation.component';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { HomeComponent } from './pages/home/home.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { HttpRequestInterceptor } from './interceptor/token.interceptor';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { LandingPageComponent } from './pages/landing-page/landing-page.component'
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [
@@ -29,7 +32,7 @@ import { LandingPageComponent } from './pages/landing-page/landing-page.componen
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    MatToolbarModule, MatButtonModule, MatIconModule, MatSnackBarModule
+    MatToolbarModule, MatButtonModule, MatIconModule, MatSnackBarModule, MatTooltipModule
   ],
   providers: [ [{provide: HTTP_INTERCEPTORS, useClass: HttpRequestInterceptor, multi: true}] ],
   bootstrap: [AppComponent]
